6aa11a52350aa8d61cd5cd750a2c38284467579b
paine leffler <pleffler@vols.utk.edu>
2017-11-26 13:20:12 -0500

    added navbar and new styling

--- src/App.js
+++ src/App.js
@@ -7,22 +7,26 @@ import Results from './Results.jsx';
 import About from './About.jsx';
 import Compare from './Compare.jsx';
 import Analytics from './Analytics.jsx';
+import NavBar from './NavBar.jsx';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <BrowserRouter>
-          <Switch>
-            <Route exact path='/about' component={About} />
-            <Route path='/results/:platform/:username' component={Results} />
-            <Route path='/compare/:user1/:user2' component={Compare} />
-            <Route path='/analytics' component={Analytics} />
-            <Route exact path='/' component={Home} />
-            <Route render={function () {
-              return <h1>Not Found</h1>
-            }} />
-          </Switch>
+          <div>
+          <NavBar />
+            <Switch>
+              <Route exact path='/about' component={About} />
+              <Route path='/results/:platform/:username' component={Results} />
+              <Route path='/compare/:user1/:user2' component={Compare} />
+              <Route path='/analytics' component={Analytics} />
+              <Route exact path='/' component={Home} />
+              <Route render={function () {
+                return <h1>Not Found</h1>
+              }} />
+            </Switch>
+          </div>
         </BrowserRouter>
       </div>
     );
